3aadb7d04f60740cef5eae423408a7b42889ffcf
João Oliveira <deoliveira.jvm@gmail.com>
2023-04-15 13:46:32 -0400

    refactor: get port from env

--- api/server.js
+++ api/server.js
@@ -8,10 +8,11 @@ server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
-server.listen(3000, () => {
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
     console.log('JSON Server is running')
 })
 
